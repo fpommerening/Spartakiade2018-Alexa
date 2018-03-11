9d36a52f56d028ea95d22d06f936ad6aa60b8508
Frank Pommerening <frank@pommerening-online.de>
2018-03-11 12:18:04 +0100

    Add OpenFaaS Backend for Hallo-World sample

---
+++ Samples/Hallo-World/Backend-OpenFaaS/hallo-world/FunctionHandler.cs
@@ -0,0 +1,68 @@
+﻿using Alexa.NET;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using OpenFaaS.Dotnet;
+
+namespace Function
+{
+    public class FunctionHandler : BaseFunction
+    {
+        public FunctionHandler(IFunctionContext functionContext)
+            : base(functionContext)
+        {
+        }
+
+          public override void Handle(string input)
+        {
+            var request = JsonConvert.DeserializeObject<SkillRequest>(input);
+
+            var requestType = request.GetRequestType();
+
+            SkillResponse response = null;
+
+            if (requestType == typeof(IntentRequest))
+            {
+                response = ExecuteIntent(request);
+            }
+            else if (requestType == typeof(LaunchRequest))
+            {
+                response = ResponseBuilder.Tell("Herzlich willkommen");
+            }
+            else if (requestType == typeof(SessionEndedRequest))
+            {
+                response = ResponseBuilder.Empty();
+            }
+
+            Context.WriteContent(JsonConvert.SerializeObject(response, Formatting.Indented,
+                new JsonSerializerSettings {ContractResolver = new CamelCasePropertyNamesContractResolver()}));
+        }
+
+        private static SkillResponse ExecuteIntent(SkillRequest skillRequest)
+        {
+            var intentRequest = skillRequest.Request as IntentRequest;
+            if (intentRequest == null)
+            {
+               return ResponseBuilder.Tell("Fehler");
+            }
+
+            if (intentRequest.Intent.Name == "greeting")
+            {
+                if (intentRequest.Intent.Slots.TryGetValue("anfrage", out var slot) && !string.IsNullOrEmpty(slot.Value))
+                {
+                    return ResponseBuilder.Tell($"Guten Tag {slot.Value}");
+                }
+
+                if (intentRequest.Intent.Slots.TryGetValue("name", out slot) && !string.IsNullOrEmpty(slot.Value))
+                {
+                    return ResponseBuilder.Tell($"Grüß Gott {slot.Value}");
+                }
+                return ResponseBuilder.Tell($"Hallo Welt");
+            }
+
+            return ResponseBuilder.Tell($"Intent {intentRequest.Intent.Name} ist unbekannt");
+        }
+    }
+}
