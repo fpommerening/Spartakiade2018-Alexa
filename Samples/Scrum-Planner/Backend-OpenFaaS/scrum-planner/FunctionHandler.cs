fccc5c0b59e3b3b0aafdd60460608513cd102430
Frank Pommerening <frank@pommerening-online.de>
2018-03-11 12:58:33 +0100

    Init  OpenFaaS backend for scrum-planner

---
+++ Samples/Scrum-Planner/Backend-OpenFaaS/scrum-planner/FunctionHandler.cs
@@ -0,0 +1,31 @@
+﻿using Alexa.NET;
+using Alexa.NET.Request;
+using Alexa.NET.Response;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using OpenFaaS.Dotnet;
+
+namespace Function
+{
+    public class FunctionHandler : BaseFunction
+    {
+        public FunctionHandler(IFunctionContext functionContext)
+            : base(functionContext)
+        {
+        }
+
+        public override void Handle(string input)
+        {
+            var request = JsonConvert.DeserializeObject<SkillRequest>(input);
+
+            var requestType = request.GetRequestType();
+
+            SkillResponse response = null;
+
+            response = ResponseBuilder.Tell($"Request-Typ {requestType.Name}");
+
+            Context.WriteContent(JsonConvert.SerializeObject(response, Formatting.Indented,
+                new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() }));
+        }
+    }
+}
