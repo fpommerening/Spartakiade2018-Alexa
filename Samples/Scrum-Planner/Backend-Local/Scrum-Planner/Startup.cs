164649aed4f344dc2b67b8ae5b4b70bd3ca14808
Frank Pommerening <frank@pommerening-online.de>
2018-03-14 22:58:58 +0100

    Add lokal backend for scrum - planner

---
+++ Samples/Scrum-Planner/Backend-Local/Scrum-Planner/Startup.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Function;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using OpenFaaS.Dotnet;
+
+
+namespace ScrumPlanner.Local
+{
+    public class Startup
+    {
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                var functionContext = new TestFuntionContext();
+                var functionHandler = new FunctionHandler(functionContext);
+                try
+                {
+                    var requestBody = await getRequest(context.Request.Body);
+                    functionHandler.Handle(requestBody);
+                    await context.Response.WriteAsync(functionContext.Content);
+                }
+                catch (Exception ex)
+                {
+                    await context.Response.WriteAsync(ex.Message);
+                }
+            });
+        }
+
+        private async Task<string> getRequest(Stream inputBody)
+        {
+            using (StreamReader reader = new StreamReader(inputBody))
+            {
+                return await reader.ReadToEndAsync();
+            }
+        }
+    }
+
+
+
+}
