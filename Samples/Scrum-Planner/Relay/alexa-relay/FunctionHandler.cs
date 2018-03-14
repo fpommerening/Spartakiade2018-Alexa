a66f09ccf7f85f3748b6b1b1a58216548ca28b62
Frank Pommerening <frank@pommerening-online.de>
2018-03-14 22:59:51 +0100

    function for alexa relay

---
+++ Samples/Scrum-Planner/Relay/alexa-relay/FunctionHandler.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Net.Http;
+
+namespace Function
+{
+    public class FunctionHandler
+    {
+        public void Handle(string input)
+        {
+            string url = Environment.GetEnvironmentVariable("relay_target_url");
+
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = client.PostAsync(url, new StringContent(input)).Result)
+                using (HttpContent content = response.Content)
+                {
+                    string result = content.ReadAsStringAsync().Result;
+                    Console.WriteLine(result);
+                }
+            }
+        }
+    }
+}
