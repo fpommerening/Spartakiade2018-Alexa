b559e56be3c32a202bfcf5a5f1d12d8bf51cb68a
Frank Pommerening <frank@pommerening-online.de>
2018-03-17 22:44:38 +0100

    starter

--- Samples/Scrum-Planner/Backend-OpenFaaS/scrum-planner/FunctionHandler.cs
+++ Samples/Scrum-Planner/Backend-OpenFaaS/scrum-planner/FunctionHandler.cs
@@ -16,48 +16,10 @@ namespace Function
         {
         }
 
-        private BaseIntentHandler[] handlers = 
-        {
-            new ParticipantAddIntentHandler(),
-            new AppointmentCreateIntentHandler(), 
-            new AppointmentListIntentHandler(), 
-
-            new AmazonYesIntentHandler(), 
-            new AmazonNoIntentHandler()
-        };
-
         public override void Handle(string input)
         {
-            var skillRequest = JsonConvert.DeserializeObject<SkillRequest>(input);
-            SkillResponse response = null;
-
-            var requestType = skillRequest.GetRequestType();
-
-            if (requestType == typeof(IntentRequest))
-            {
-                response = HandleIntentRequest(skillRequest);
-            }
-            else
-            {
-                response = ResponseBuilder.Tell($"Request-Typ {requestType.Name}");
-            }
-
-            Context.WriteContent(JsonConvert.SerializeObject(response, Formatting.Indented,
-                    new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver()}));
         }
 
-        private SkillResponse HandleIntentRequest(SkillRequest skillRequest)
-        {
-            var intentRequest = (IntentRequest)skillRequest.Request;
-            foreach (var handler in handlers)
-            {
-                if (handler.IntentName == intentRequest.Intent.Name)
-                {
-                    return handler.Handle(skillRequest);
-                }
-            }
-            return ResponseBuilder.Tell($"Der Aufruf {intentRequest.Intent.Name} ist unbekannt.");
-        }
        
     }
 }
